0fb94fc4763b319b023d7230b20158aabadd2eed
Christopher Kapic <christopherkapic@gmail.com>
2022-05-03 20:31:31 -0500

    logging

--- pg_capup.py
+++ pg_capup.py
@@ -7,8 +7,12 @@ from datetime import datetime
 
 f = open(f'{os.path.expanduser("~")}/pg_capup/config.json')
 config = json.load(f)
+current_time = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+
+print(f"Backing up databases at {current_time}.")
 
 def backup_db(database):
+  print(f"Creating backup of {database['name']}.")
   os.system(f'docker exec $(docker ps -aqf name="{database["name"]}") pg_dump -U {database["username"]} -Fc {database["database"]} > {config["working_path"]}{database["name"]}.sql')
 
 def upload_helper(database, location):
@@ -19,7 +23,7 @@ def upload_helper(database, location):
       aws_access_key_id = location["connection"]["aws_access_key_id"],
       aws_secret_access_key = location["connection"]["aws_secret_access_key"]
     )
-    s3.Bucket(location["connection"]["bucket"]).upload_file(f'{config["working_path"]}{database["name"]}.sql', f"{database['name']}-{datetime.today().strftime('%Y-%m-%d-%H:%M:%S')}.sql")
+    s3.Bucket(location["connection"]["bucket"]).upload_file(f'{config["working_path"]}{database["name"]}.sql', f"{database['name']}-{current_time}.sql")
   else:
     return
 
@@ -28,6 +32,7 @@ def upload_file(database, location, locations):
     if loc["name"] != location:
       continue
     elif loc["name"] == location:
+      print(f"Uploading backup of {database['name']} to {location}.")
       upload_helper(database, loc)
 
 
